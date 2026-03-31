cbeba84f083ec07534eae460680893c5a04c9ae0
Wudzel <leochronister44@gmail.com>
2026-03-31 18:46:00 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,26 @@ import sys
 
 token = os.environ["DISCORD_TOKEN"]
 channel_id = int(os.environ["DISCORD_CHANNEL_ID"])
-files_to_send = sys.argv[1:]  # files passed in as arguments
+
+# Split the single string of files into a list
+files_to_send = sys.argv[1].split() if len(sys.argv) > 1 else []
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    channel = client.get_channel(channel_id)
+    channel = await client.fetch_channel(channel_id)
+    if not files_to_send:
+        print("No files to send")
+        await client.close()
+        return
     for filepath in files_to_send:
         if os.path.exists(filepath):
             await channel.send(file=discord.File(filepath))
             print(f"Sent: {filepath}")
+        else:
+            print(f"File not found: {filepath}")
     await client.close()
 
 client.run(token)
